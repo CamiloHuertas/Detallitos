d45d25d2f037703285a9466d9e1198160463c863
Camilo Huertas <camiloandreshuertas26@gmail.com>
2021-12-02 23:16:48 -0500

    menuLinks

--- JS/home.js
+++ JS/home.js
@@ -6,3 +6,37 @@ btn.addEventListener("click", () => {
   btn.classList.toggle("is-active")
   menu.classList.toggle("active")
 })
+
+// MenuMovile links
+// Events
+
+const events = d.querySelector(".events"),
+      presents = d.querySelector(".presents"),
+      jewelery = d.querySelector(".jewelery"),
+      clothes = d.querySelector(".clothes"),
+      spa = d.querySelector(".spa"),
+      curiosities = d.querySelector(".curiosities")
+
+const menuLinks = d.querySelectorAll(".links")
+  
+events.addEventListener("click", () => {
+  menuLinks[0].classList.toggle("active")
+})
+  
+presents.addEventListener("click", () => {
+  menuLinks[1].classList.toggle("active")
+})
+jewelery.addEventListener("click", () => {
+  menuLinks[2].classList.toggle("active")
+})
+clothes.addEventListener("click", () => {
+  menuLinks[3].classList.toggle("active")
+})
+spa.addEventListener("click", () => {
+  menuLinks[4].classList.toggle("active")
+})
+curiosities.addEventListener("click", () => {
+  menuLinks[5].classList.toggle("active")
+})
+
+//
