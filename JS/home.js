5e5384b2363a7010fd896164849acef6fe997776
Camilo Huertas <camiloandreshuertas26@gmail.com>
2021-12-02 21:34:56 -0500

    menuMovile

---
+++ JS/home.js
@@ -0,0 +1,8 @@
+const d = document
+const btn = d.querySelector(".hamburger")
+const menu = d.querySelector(".menuMovile")
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("is-active")
+  menu.classList.toggle("active")
+})
